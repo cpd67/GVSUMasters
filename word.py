b1bb17145c511167c47a0f934e8fb2decfd4af3a
Chris Dilley <codemonkey941@gmail.com>
2021-04-03 13:46:51 -0400

    Improve word generation script.

--- word.py
+++ word.py
@@ -1,5 +1,6 @@
+# https://stackoverflow.com/questions/3277503/how-to-read-a-file-line-by-line-into-a-list
 def generate_word_file():
-    """ Generate a large file containing words across 10 books. """
+    """ Generate a large file containing words across 15 books. """
     text_files = [
                 './textFiles/PrideAndPrejudice.txt', 
                 './textFiles/ATaleOfTwoCities.txt', 
@@ -18,14 +19,12 @@ def generate_word_file():
                 './textFiles/Ulysses.txt',                
                 ]
 
-    with open('./words.txt', 'w') as f2:
+    with open('./words.txt', 'w') as f:
         for file in text_files:
-            with open(file, 'r') as f:
-                line = f.readline()
-                while line != '':
-                    words = line.replace("\n", '').split(' ')
-                    for word in words:
+            with open(file, 'r') as f2:
+                lines = [line.replace("\n", '') for line in f2.readlines()]
+                for line in lines:
+                    for word in line.split(' '):
+                        word = word.strip('-\'\";\.,()”“?!_:_—\.’‘').lower()
                         if len(word) > 0:
-                            word = word.strip('-\'\";\.,()”“?!_:_—\.’‘').lower()
-                            f2.write(f'{word}\n')              
-                    line = f.readline()
+                            f.write(f'{word}\n')
