90b159148c852df7d7577d30e3e1b47734a9c06a
Chris Dilley <codemonkey941@gmail.com>
2021-04-05 20:12:11 -0400

    Time each query in experiment-2.py & tweak experiment-1.py some more.

--- experiment-2.py
+++ experiment-2.py
@@ -29,51 +29,87 @@ start = time.time()
 stop = list(set(stopwords.words('english')))
 generate_word_file()
 
+query_times = []
 df = spark.read.text("./words.txt")
 
 print("Query #1: words that do not start with 'a'")
+query_start = time.time()
 df.select('value').filter(~df.value.like('a%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(~df.value.like('a%')).distinct().count()}")
+word_count = df.select('value').filter(~df.value.like('a%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #2: words that do start with 'a'")
+query_start = time.time()
 df.select('value').filter(df.value.like('a%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('a%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('a%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #3: words that contain 'ab'")
+query_start = time.time()
 df.select('value').filter(df.value.like('%ab%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('%ab%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('%ab%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #4: words that contain 'c'")
+query_start = time.time()
 df.select('value').filter(df.value.like('%c%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('%c%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('%c%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #5: words that contain 'f'")
+query_start = time.time()
 df.select('value').filter(df.value.like('%f%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('%f%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('%f%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #6: words that end in 'z'")
+query_start = time.time()
 df.select('value').filter(df.value.like('%z')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('%z')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('%z')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #7: 8-letter words that end in 'ing'")
+query_start = time.time()
 df.select('value').filter(df.value.like('_____ing')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('_____ing')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('_____ing')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #8: 4-letter words that start with y")
+query_start = time.time()
 df.select('value').filter(df.value.like('y___')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('y___')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('y___')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #9: Words where the third character is 'i'")
+query_start = time.time()
 df.select('value').filter(df.value.like('__i%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('__i%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('__i%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 print("Query #10: words that start with 'g', and where the fifth character is a 'e'")
+query_start = time.time()
 df.select('value').filter(df.value.like('g___e%')).distinct().show(10)
-print(f"Number of words: {df.select('value').filter(df.value.like('g___e%')).distinct().count()}")
+word_count = df.select('value').filter(df.value.like('g___e%')).distinct().count()
+query_times.append(time.time() - query_start)
+print(f"Number of words: {word_count}")
 
 end = time.time() - start
 
 # Cleanup
 spark.stop()
 
-print(f'\nEnd time: {end}\n')
+print("----------------------------")
+for i, query_t in enumerate(query_times, start=1):
+    print(f"Query {i} time: {query_t}s")
+
+print(f'\nProgram run time: {end}s\n')
+print("----------------------------")
