388f315a7bd8ca8d7cd10715cddb01e5bc4f2f31
Chris Dilley <codemonkey941@gmail.com>
2021-04-10 12:29:56 -0400

    Final cleanup & tweaks before getting times.

--- experiment-1.py
+++ experiment-1.py
@@ -1,16 +1,24 @@
 """ Experiment #1: Fun with words
 
-    Perform several spark queries on a large list of words, gathered across 15 books.
+    Perform several Spark SQL queries on a large list of words, gathered across 15 books.
 
     The following resources were helpful when writing this program:
+        * https://nvidia.github.io/spark-rapids/
         * https://www.tutorialspoint.com/python_text_processing/python_remove_stopwords.htm
         * https://stackoverflow.com/questions/40287237/pyspark-dataframe-operator-is-not-in
         * https://spark.apache.org/docs/latest/api/python/reference/index.html
+        * http://www.nltk.org/api/nltk.html
+        * https://www.tutorialspoint.com/python_text_processing/python_remove_stopwords.htm
+        * https://gist.github.com/sebleier/554280
+        * https://stackoverflow.com/questions/28189408/how-to-reduce-the-verbosity-of-sparks-runtime-output
+        * https://spark.apache.org/docs/latest/sql-getting-started.html
+        * https://spark.apache.org/docs/latest/api/python/_modules/pyspark/sql/readwriter.html
 """
 
 import time
 
 import nltk
+# Download and use the stopwords package
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from pyspark.sql import SparkSession
@@ -18,32 +26,31 @@ from pyspark.sql import SparkSession
 from word import generate_word_file
 
 
+# Create a SparkSession instance, the entry point for all spark functionality
 spark = SparkSession.builder.appName("Experiment 1").getOrCreate()
-# There's also a log4j.properties file that controls the logging output of spark globally: https://stackoverflow.com/questions/28189408/how-to-reduce-the-verbosity-of-sparks-runtime-output
 spark.sparkContext.setLogLevel("ERROR")
 
 # Turn on spark-rapids
 spark.conf.set('spark.rapids.sql.enabled', 'true')
 
-
-# Get list of stop words to use & generate data to use
+# Get list of stop words to use & generate data
 stop = list(set(stopwords.words('english')))
 generate_word_file()
 
 query_times = []
+total_start = time.time()
 
-start = time.time()
-
+# Read in generated text file, creating a DataFrame with a 'value' column, and group by 'value', obtaining a count for each word
 df = spark.read.text("./words.txt")
 not_stop_words = df.select('value').filter(~df.value.isin(stop)).groupBy('value').count()
 are_stop_words = df.select('value').filter(df.value.isin(stop)).groupBy('value').count()
 
-print("Query #1: get 3 words that appear most frequently")
+print("Query #1: get 3 words that appear most frequently and are not stop words")
 query_start = time.time()
 not_stop_words.orderBy('count', ascending=False).show(3)
 query_times.append(time.time() - query_start)
 
-print("Query #2: get 3 words that do not appear most frequently")
+print("Query #2: get 3 words that do not appear most frequently and are not stop words")
 query_start = time.time()
 not_stop_words.orderBy('count', ascending=True).show(3)
 query_times.append(time.time() - query_start)
@@ -70,12 +77,12 @@ query_start = time.time()
 df.select('value').filter(df.value.isin('how', 'now', 'brown', 'cow')).groupBy('value').count().orderBy('count', ascending=False).show()
 query_times.append(time.time() - query_start)
 
-print("Query #7: do any of the following names appear? Sam, James, Joe, Chris")
+print("Query #7: do any of the following names appear? Sam, James, Carl, Joe, Chris")
 query_start = time.time()
 df.select('value').filter(df.value.isin('sam', 'james', 'carl', 'joe', 'chris')).groupBy('value').count().orderBy('count', ascending=False).show()
 query_times.append(time.time() - query_start)
 
-print("Query #8: do these words appear? 'mother-in-law', 'father-in-law', 'just-in-case'?")
+print("Query #8: do these words appear? 'savior', 'glory', 'mother-in-law', 'father-in-law', 'just-in-case'?")
 query_start = time.time()
 df.select('value').filter(df.value.isin('savior', 'glory', 'mother-in-law', 'father-in-law', 'just-in-case')).groupBy('value').count().orderBy('count', ascending=False).show()
 query_times.append(time.time() - query_start)
@@ -90,7 +97,7 @@ query_start = time.time()
 df.select('value').filter(df.value.isin('cat', 'dog', 'bear', 'cow', 'lizard', 'hamster', 'peacock')).groupBy('value').count().orderBy('count', ascending=False).show()
 query_times.append(time.time() - query_start)
 
-end = time.time() - start
+total_end = time.time() - total_start
 
 # Cleanup
 spark.stop()
@@ -99,5 +106,5 @@ print("----------------------------")
 for i, query_t in enumerate(query_times, start=1):
     print(f"Query {i} time: {query_t}s")
 
-print(f'\nProgram run time: {end}s\n')
+print(f'\nTotal time to read in file & run all queries: {total_end}s\n')
 print("----------------------------")
--- experiment-2.py
+++ experiment-2.py
@@ -1,14 +1,22 @@
 """ Experiment #2: Character Frequency Analysis
 
-    Perform several spark queries on a large list of words, gathered across 15 books.
+    Perform several Spark SQL queries on a large list of words, gathered across 15 books.
 
     The following resources were helpful when writing this program:
+        * https://nvidia.github.io/spark-rapids/
         * https://spark.apache.org/docs/latest/api/python/reference/index.html
         * https://www.sqlservertutorial.net/sql-server-basics/sql-server-like/
+        * http://www.nltk.org/api/nltk.html
+        * https://www.tutorialspoint.com/python_text_processing/python_remove_stopwords.htm
+        * https://gist.github.com/sebleier/554280
+        * https://stackoverflow.com/questions/28189408/how-to-reduce-the-verbosity-of-sparks-runtime-output
+        * https://spark.apache.org/docs/latest/sql-getting-started.html
+        * https://spark.apache.org/docs/latest/api/python/_modules/pyspark/sql/readwriter.html
 """
 import time
 
 import nltk
+# Download and use the stopwords package
 nltk.download('stopwords')
 from nltk.corpus import stopwords
 from pyspark.sql import SparkSession
@@ -16,8 +24,8 @@ from pyspark.sql import SparkSession
 from word import generate_word_file
 
 
+# Create a SparkSession instance, the entry point for all spark functionality
 spark = SparkSession.builder.appName("Experiment 2").getOrCreate()
-# There's also a log4j.properties file that controls the logging output of spark globally: https://stackoverflow.com/questions/28189408/how-to-reduce-the-verbosity-of-sparks-runtime-output
 spark.sparkContext.setLogLevel("ERROR")
 
 # Turn on spark-rapids
@@ -28,82 +36,92 @@ stop = list(set(stopwords.words('english')))
 generate_word_file()
 
 query_times = []
+total_start = time.time()
 
-start = time.time()
-
+# Read in generated text file, creating a DataFrame with a 'value' column
 df = spark.read.text("./words.txt")
 
 print("Query #1: words that do not start with 'a'")
 query_start = time.time()
-df.select('value').filter(~df.value.like('a%')).distinct().show(10)
-word_count = df.select('value').filter(~df.value.like('a%')).distinct().count()
+query = df.select('value').filter(~df.value.like('a%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #2: words that do start with 'a'")
 query_start = time.time()
-df.select('value').filter(df.value.like('a%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('a%')).distinct().count()
+query = df.select('value').filter(df.value.like('a%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #3: words that contain 'ab'")
 query_start = time.time()
-df.select('value').filter(df.value.like('%ab%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('%ab%')).distinct().count()
+query = df.select('value').filter(df.value.like('%ab%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #4: words that contain 'c'")
 query_start = time.time()
-df.select('value').filter(df.value.like('%c%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('%c%')).distinct().count()
+query = df.select('value').filter(df.value.like('%c%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #5: words that contain 'f'")
 query_start = time.time()
-df.select('value').filter(df.value.like('%f%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('%f%')).distinct().count()
+query = df.select('value').filter(df.value.like('%f%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #6: words that end in 'z'")
 query_start = time.time()
-df.select('value').filter(df.value.like('%z')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('%z')).distinct().count()
+query = df.select('value').filter(df.value.like('%z')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #7: 8-letter words that end in 'ing'")
 query_start = time.time()
-df.select('value').filter(df.value.like('_____ing')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('_____ing')).distinct().count()
+query = df.select('value').filter(df.value.like('_____ing')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #8: 4-letter words that start with y")
 query_start = time.time()
-df.select('value').filter(df.value.like('y___')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('y___')).distinct().count()
+query = df.select('value').filter(df.value.like('y___')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
 print("Query #9: Words where the third character is 'i'")
 query_start = time.time()
-df.select('value').filter(df.value.like('__i%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('__i%')).distinct().count()
+query = df.select('value').filter(df.value.like('__i%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
-print("Query #10: words that start with 'g', and where the fifth character is a 'e'")
+print("Query #10: words that start with 'g', and where the fifth character is an 'e'")
 query_start = time.time()
-df.select('value').filter(df.value.like('g___e%')).distinct().show(10)
-word_count = df.select('value').filter(df.value.like('g___e%')).distinct().count()
+query = df.select('value').filter(df.value.like('g___e%')).distinct()
+query.show(10)
+word_count = query.count()
 query_times.append(time.time() - query_start)
 print(f"Number of words: {word_count}")
 
-end = time.time() - start
+total_end = time.time() - total_start
 
 # Cleanup
 spark.stop()
@@ -112,5 +130,5 @@ print("----------------------------")
 for i, query_t in enumerate(query_times, start=1):
     print(f"Query {i} time: {query_t}s")
 
-print(f'\nProgram run time: {end}s\n')
+print(f'\nTotal time to read in file & run all queries: {total_end}s\n')
 print("----------------------------")
