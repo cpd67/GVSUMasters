ecd7e6a28790dbaea6e344d2d0ab0dc95fd7918a
Chris Dilley <codemonkey941@gmail.com>
2021-04-10 22:18:21 -0400

    Add timings for running each experiment in isolation.

---
+++ main-isolation.py
@@ -0,0 +1,16 @@
+import nltk
+# Download and use the stopwords package
+nltk.download('stopwords')
+
+from experiments import experiment_1, experiment_2
+from word import generate_word_file
+
+
+if __name__ == '__main__':
+    print("Generating word file....")
+    generate_word_file()
+    
+    # experiment_1(rapids_off=True)
+    # experiment_2(rapids_off=True)
+    # experiment_1()
+    experiment_2()
